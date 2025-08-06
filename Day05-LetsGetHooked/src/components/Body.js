7c37e2993b705f54d2d8792abb9c1d610cabbe59
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2025-08-07 03:16:07 +0530

    Learnt about React Hooks especially about useState

--- Day05-LetsGetHooked/src/components/Body.js
+++ Day05-LetsGetHooked/src/components/Body.js
@@ -1,17 +1,34 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    // Local state variable - Super powerful variable
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
+
     return (
         <div className='body'>
-            <div className='search'>Search Bar</div>
+            <div className='search'>
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        console.log('btn pressed');                        
+                        const filteredList = listOfRestaurants.filter(
+                            (restaurant) => restaurant.data.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredList);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className='res-container'>
-                {restaurantList.map((restaurant) => {
+                {listOfRestaurants.map((restaurant) => {
                     return <RestaurantCard key={restaurant.data.id} resData={restaurant} />
                 })}
             </div>
         </div>
-    )
+    );
 };
 
 export default Body;
