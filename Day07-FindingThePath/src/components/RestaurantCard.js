b187daf446cd241355a7ff1a93bf20b240ec5c6b
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2025-08-08 01:51:47 +0530

    Learnt about handling routes in React

---
+++ Day07-FindingThePath/src/components/RestaurantCard.js
@@ -0,0 +1,31 @@
+import { IMAGE_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        avgRating,
+        costForTwoString,
+        deliveryTime,
+    } = resData?.info;
+    return (
+        <div className='res-card' style={{backgroundColor: "#f0f0f0"}}>
+            <img
+                className='res-logo'
+                alt='res-logo'
+                src={IMAGE_URL + cloudinaryImageId}
+                width={280}
+                height={250}
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <p>{avgRating} stars</p>
+            <p>{costForTwoString}</p>
+            <p>{deliveryTime} minutes</p>
+        </div>
+    )
+};
+
+export default RestaurantCard;
